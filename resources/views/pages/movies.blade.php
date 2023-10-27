13a9f72818bd8cd8d46bb4e7435f2b36bc30e306
Riccardo <riky.volto@live.com>
2023-10-27 18:55:50 +0200

    modify layout

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -2,20 +2,33 @@
 
 @section('main_content')
 
-@foreach ($Movies as $movie)
+<div class="container">
+    <div class="row">
+        <div class="col d-flex flex-wrap">
+            
+            @foreach ($Movies as $movie)
 
-<div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{$movie->title}}</h5>
-      <div class="card-text">{{$movie->original_title}}</div>
-      <div>{{$movie->nationality}}</div>
-      <div>{{$movie->date}}</div>
-      <div>{{$movie->vote}}</div>
+            <div class="card m-3 text-white bg-primary" style="width: 18rem;">
+                <img src="https://picsum.photos/200/300" class="card-img-top" alt="...">
+                <div class="card-body">
+                <div class="card-title"><h5>Titolo:</h5>  {{$movie->title}}</div>
+                <div class="card-text"><h5>Titolo originale:</h5>  {{$movie->original_title}}</div>
+                <div><h5>Nazionalità:</h5>  {{$movie->nationality}}</div>
+                <div><h5>Data pubblicazione:</h5>  {{$movie->date}}</div>
+                <div><h5>Voto:</h5>  {{$movie->vote}}</div>
+                </div>
+            </div>
+
+
+            @endforeach
+        </div>
     </div>
-  </div>
+</div>
 
-
-@endforeach
+<style>
+    body {
+        background-color: black
+    }
+</style>
 
 @endsection

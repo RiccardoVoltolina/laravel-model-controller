3e1850444e2bcf954a3b4325d4d253a802879404
Riccardo <riky.volto@live.com>
2023-10-27 18:37:28 +0200

    print all movie info inside card

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -4,7 +4,17 @@
 
 @foreach ($Movies as $movie)
 
-<div>{{$movie->title}}</div>
+<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">{{$movie->title}}</h5>
+      <div class="card-text">{{$movie->original_title}}</div>
+      <div>{{$movie->nationality}}</div>
+      <div>{{$movie->date}}</div>
+      <div>{{$movie->vote}}</div>
+    </div>
+  </div>
+
 
 @endforeach
 
